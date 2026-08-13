829907697552971fa000cf621fff1eb71516f3f5
crissticrs <rptfzp5pft@privaterelay.appleid.com>
2026-08-13 22:51:47 +0300

    SW v23: scene editor canvas-top horizontal bar layout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* LuaX service worker */
-const CACHE_VERSION = 'luax-v22';
+const CACHE_VERSION = 'luax-v23';
 const SHELL_CACHE = CACHE_VERSION + '-shell';
 const CDN_CACHE = CACHE_VERSION + '-cdn';
 
