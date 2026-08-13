be3aaadd5ed691006fcecca140d71bc9eda7491e
crissticrs <rptfzp5pft@privaterelay.appleid.com>
2026-08-13 22:12:25 +0300

    SW v20: scene editor exit freeze fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* LuaX service worker */
-const CACHE_VERSION = 'luax-v19';
+const CACHE_VERSION = 'luax-v20';
 const SHELL_CACHE = CACHE_VERSION + '-shell';
 const CDN_CACHE = CACHE_VERSION + '-cdn';
 
