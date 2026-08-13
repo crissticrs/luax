ba473543f23d7883682c9eddb5123fc1b4556b32
crissticrs <rptfzp5pft@privaterelay.appleid.com>
2026-08-13 23:28:45 +0300

    SW v27: restore classic scene toolbar + emoji tabs only

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* LuaX service worker */
-const CACHE_VERSION = 'luax-v26';
+const CACHE_VERSION = 'luax-v27';
 const SHELL_CACHE = CACHE_VERSION + '-shell';
 const CDN_CACHE = CACHE_VERSION + '-cdn';
 
