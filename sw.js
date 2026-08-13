230c248f96639b7199b192a5a592b5448f4b7cb7
crissticrs <rptfzp5pft@privaterelay.appleid.com>
2026-08-13 19:57:57 +0300

    SW v19: pixel-grid scene paint + spectrum

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* LuaX service worker */
-const CACHE_VERSION = 'luax-v18';
+const CACHE_VERSION = 'luax-v19';
 const SHELL_CACHE = CACHE_VERSION + '-shell';
 const CDN_CACHE = CACHE_VERSION + '-cdn';
 
