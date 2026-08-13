ba1f48fb56a27c60e3308676e6e6b0b02dbcc00b
crissticrs <rptfzp5pft@privaterelay.appleid.com>
2026-08-13 23:19:50 +0300

    SW v26: restore paint + spectrum-only polish

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* LuaX service worker */
-const CACHE_VERSION = 'luax-v25';
+const CACHE_VERSION = 'luax-v26';
 const SHELL_CACHE = CACHE_VERSION + '-shell';
 const CDN_CACHE = CACHE_VERSION + '-cdn';
 
