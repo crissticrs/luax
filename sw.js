01ca4637713c718e64c8be0b6a9dc23b4a469f57
crissticrs <rptfzp5pft@privaterelay.appleid.com>
2026-08-13 23:38:58 +0300

    SW v28: scene toolbar layout rearrange

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* LuaX service worker */
-const CACHE_VERSION = 'luax-v27';
+const CACHE_VERSION = 'luax-v28';
 const SHELL_CACHE = CACHE_VERSION + '-shell';
 const CDN_CACHE = CACHE_VERSION + '-cdn';
 
