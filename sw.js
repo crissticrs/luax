517e2fc128b47a0dc0b4140ac3086aba553469a3
crissticrs <rptfzp5pft@privaterelay.appleid.com>
2026-08-13 17:47:21 +0300

    SW v14: swipe snap fix + file pin

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* LuaX service worker */
-const CACHE_VERSION = 'luax-v13';
+const CACHE_VERSION = 'luax-v14';
 const SHELL_CACHE = CACHE_VERSION + '-shell';
 const CDN_CACHE = CACHE_VERSION + '-cdn';
 
