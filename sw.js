99db8cbcb83ecb1d82ffb897b8eecf1f49ab8e7c
crissticrs <rptfzp5pft@privaterelay.appleid.com>
2026-08-13 19:10:00 +0300

    SW v18: scene editor paint + pinch zoom

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* LuaX service worker */
-const CACHE_VERSION = 'luax-v17';
+const CACHE_VERSION = 'luax-v18';
 const SHELL_CACHE = CACHE_VERSION + '-shell';
 const CDN_CACHE = CACHE_VERSION + '-cdn';
 
